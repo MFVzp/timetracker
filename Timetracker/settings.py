62a5681f3bc6aa411b8e25b55a5f09c17a1700cc
Ilya <tauruszpua@yahoo.com>
2018-06-28 09:11:00 +0300

    add WorkDiary and ScreenShot models

--- Timetracker/settings.py
+++ Timetracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'work_diary',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Image storage
+IMAGE_STORAGE_FOLDER = 'images'
--- work_diary/models.py
+++ work_diary/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your models here.
+
+def get_image_path(instance, filename):
+    return '{}/{}/{}'.format(settings.IMAGE_STORAGE_FOLDER, instance.work_diary.user.username, filename)
+
+
+class WorkDiary(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='work_diaries')
+    
+
+class ScreenShot(models.Model):
+    work_diary = models.ForeignKey(WorkDiary, on_delete=models.CASCADE, related_name='screenshots')
+    
+    image = models.ImageField(upload_to=get_image_path)
+    description = models.TextField()
+    create_date = models.DateTimeField()
